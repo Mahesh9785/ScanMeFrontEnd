95bfb1af4bb44be81a9a6428f25b580b10a42ff5
Mahesh Sharma <Mahesh9785>
2024-05-09 17:41:22 +0530

    url updated

--- src/app/Environments/environments.prod.ts
+++ src/app/Environments/environments.prod.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
     production: true,
-    apiUrl: '3.110.103.212:3000/api', // Production API endpoint
+    apiUrl: 'http://http://ec2-3-110-103-212.ap-south-1.compute.amazonaws.com/:3000/api', // Production API endpoint
     debugMode: false,
     // Other production-specific variables
   };
