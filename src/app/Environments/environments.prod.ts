438102be5dbf1bdc0fe314c6f68090df1710b386
Mahesh Sharma <Mahesh9785>
2024-05-09 17:33:30 +0530

    url updated

--- src/app/Environments/environments.prod.ts
+++ src/app/Environments/environments.prod.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
     production: true,
-    apiUrl: '3.6.89.195:3000/api', // Production API endpoint
+    apiUrl: '3.110.103.212:3000/api', // Production API endpoint
     debugMode: false,
     // Other production-specific variables
   };
